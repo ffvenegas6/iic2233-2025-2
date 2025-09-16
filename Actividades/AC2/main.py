e59ab46ab6dc6de5ded2d39ca5676ee9063b7c36
Felipe Venegas <pipevm98@gmail.com>
2025-09-16 17:16:40 -0300

    estudios_con_genero testeada con exito

--- Actividades/AC2/main.py
+++ Actividades/AC2/main.py
@@ -82,8 +82,14 @@ def resumen_animes_por_ver(*animes: Anime) -> dict:
 
 def estudios_con_genero(genero: str, **estudios: dict) -> list:
     # TODO: Completar
-    
-    return
+    estudios_list = []
+    # Iteramos sobre los estudios y sus animes
+    if estudios:
+        for estudio, animes in estudios.items():
+            # Si algún anime del estudio tiene el género buscado, agregamos el estudio a la lista
+            if any(genero in anime.generos for anime in animes):
+                estudios_list.append(estudio)
+    return estudios_list
 
 
 if __name__ == "__main__":
@@ -96,45 +102,45 @@ if __name__ == "__main__":
         print(f"{indice} - {anime}")
         indice += 1
 
-    # #####################################
-    # #        Parte II - Consultas       #
-    # #####################################
-    # # Solo se usará los 2 animes del enunciado.
-    # datos = [
-    #     Anime(
-    #         nombre="Hunter x Hunter",
-    #         capitulos=62,
-    #         puntaje=9,
-    #         estreno=1999,
-    #         estudio="Nippon Animation",
-    #         generos={"Aventura", "Comedia", "Shonen", "Acción"},
-    #     ),
-    #     Anime(
-    #         nombre="Sakura Card Captor",
-    #         capitulos=70,
-    #         puntaje=10,
-    #         estreno=1998,
-    #         estudio="Madhouse",
-    #         generos={"Shoujo", "Comedia", "Romance", "Acción"},
-    #     ),
-    # ]
+    #####################################
+    #        Parte II - Consultas       #
+    #####################################
+    # Solo se usará los 2 animes del enunciado.
+    datos = [
+        Anime(
+            nombre="Hunter x Hunter",
+            capitulos=62,
+            puntaje=9,
+            estreno=1999,
+            estudio="Nippon Animation",
+            generos={"Aventura", "Comedia", "Shonen", "Acción"},
+        ),
+        Anime(
+            nombre="Sakura Card Captor",
+            capitulos=70,
+            puntaje=10,
+            estreno=1998,
+            estudio="Madhouse",
+            generos={"Shoujo", "Comedia", "Romance", "Acción"},
+        ),
+    ]
 
-    # # animes_por_estreno
-    # estrenos = animes_por_estreno(datos)
-    # print(estrenos)
+    # animes_por_estreno
+    estrenos = animes_por_estreno(datos)
+    print(estrenos)
 
-    # # descartar_animes
-    # animes = descartar_animes({"Comedia", "Horror"}, datos)
-    # print(animes)
+    # descartar_animes
+    animes = descartar_animes({"Comedia", "Horror"}, datos)
+    print(animes)
 
-    # # resumen_animes_por_ver
-    # resumen = resumen_animes_por_ver(datos[0], datos[1])
-    # print(resumen)
+    # resumen_animes_por_ver
+    resumen = resumen_animes_por_ver(datos[0], datos[1])
+    print(resumen)
 
-    # # estudios_con_genero
-    # estudios = estudios_con_genero(
-    #     "Shonen",
-    #     Nippon_Animation=[datos[0]],
-    #     Madhouse=[datos[1]],
-    # )
-    # print(estudios)
+    # estudios_con_genero
+    estudios = estudios_con_genero(
+        "Shonen",
+        Nippon_Animation=[datos[0]],
+        Madhouse=[datos[1]],
+    )
+    print(estudios)
